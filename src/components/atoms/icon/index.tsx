6811a87114016afdd9ba78d05758d72bf8a6a7f7
Lucas Panizio <lucas_panizio@hotmail.com>
2025-01-12 00:53:05 -0300

    💄 style: add background property to icon component

--- src/components/atoms/icon/index.tsx
+++ src/components/atoms/icon/index.tsx
@@ -11,15 +11,34 @@ export interface IconProps {
   name: IconName;
   size?: icons.IconProps['size'];
   color?: keyof ThemeType['colors'];
+  background?: keyof ThemeType['colors'];
   weight?: icons.IconWeight;
 }
 
-export const Icon = ({ name, color, weight, size = 22 }: IconProps) => {
+export const Icon = ({ name, color, background, weight, size = 22 }: IconProps) => {
   const IconComponent = icons[name] as icons.Icon;
 
   if (!IconComponent) return null;
 
   const colorValue = colorToCSSValue(color);
 
-  return <IconComponent size={size} color={colorValue} weight={weight} />;
+  const backgroundStyle = background && {
+    background: colorToCSSValue(background),
+    padding: calculatePadding(size),
+    borderRadius: '50%',
+  };
+
+  return <IconComponent size={size} color={colorValue} weight={weight} style={backgroundStyle} />;
 };
+
+function calculatePadding(size?: string | number) {
+  if (!size) return undefined;
+  if (typeof size === 'number') return size / 4;
+
+  if (typeof size === 'string') {
+    if (size.includes('px')) return `${parseFloat(size) / 4}px`;
+    if (size.includes('rem')) return `${(parseFloat(size) * 16) / 4}px`;
+  }
+
+  return undefined;
+}
